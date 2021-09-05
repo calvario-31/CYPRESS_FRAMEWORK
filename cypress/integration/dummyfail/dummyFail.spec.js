fd4b6115492ead807f99f759247b16da989c76f3
calvario31 <cvargasr.31@gmail.com>
2021-09-05 16:33:37 -0500

    removed failed

--- cypress/integration/dummyfail/dummyFail.spec.js
+++
@@ -1,8 +0,0 @@
-import {mainPage} from "../../support/pageobjects/credentials/Main.page";
-
-describe('dummy fail', () => {
-    it('test to see the screenshot', () => {
-        cy.login()
-        mainPage.verifyPage()
-    })
-})
